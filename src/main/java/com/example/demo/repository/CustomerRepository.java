46e4fe7f8ad0f0f682eb6872cd17b8eaf369bb4c
rhodrigo081 <rhodrigo.leandro@gmail.com>
2025-10-01 19:31:36 -0300

    feat: difference between exact search and containing search

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -4,12 +4,19 @@ import com.example.demo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-    Optional<Customer> findCustomerByCPF(String cpf);
-    Optional<Customer> findCustomerByEmail(String email);
-    Optional<Customer> findCustomerByPhone(String phone);
+    List<Customer> findCustomerByCPFContaining(String cpf);
+    List<Customer> findCustomerByEmailContaining(String email);
+    List<Customer> findCustomerByPhoneContaining(String phone);
+    List<Customer> findCustomerByNameContaining(String name);
+
+    Optional<Customer> findCustomerByExactCPF(String exactCPF);
+    Optional<Customer> findCustomerByExactEmail(String exactEmail);
+    Optional<Customer> findCustomerByExactPhone(String exactPhone);
+    Optional<Customer> findCustomerByExactName(String exactName);
 }
--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.UUID;
 
 @Service
 public class CustomerService {
@@ -21,15 +21,15 @@ public class CustomerService {
     @Transactional
     public Customer customerRegister(CustomerRecordDto customerDTO) {
         try {
-            if(customerDTO == null){
+            if (customerDTO == null) {
                 throw new IllegalArgumentException("Fill all fields");
             }
 
-            Customer customerByCPF = findCustomerByCPF(customerDTO.cpf());
-            Customer customerByEmail = findCustomerByEmail(customerDTO.email());
-            Customer customerByPhone = findCustomerByPhone(customerDTO.phone());
+            Customer customerByCPF = findCustomerByExactCPF(customerDTO.cpf());
+            Customer customerByEmail = findCustomerByExactEmail(customerDTO.email());
+            Customer customerByPhone = findCustomerByExactPhone(customerDTO.phone());
 
-            if(customerByCPF != null ||  customerByEmail != null || customerByPhone != null){
+            if (customerByCPF != null || customerByEmail != null || customerByPhone != null) {
                 throw new IllegalArgumentException("User Already Exists");
             }
 
@@ -44,36 +44,96 @@ public class CustomerService {
     }
 
     @Transactional(readOnly = true)
-    public Customer findCustomerByCPF(String cpf) {
+    public List<Customer> findCustomerByCPFContaining(String cpf) {
         try {
-            return customerRepository.findCustomerByCPF(cpf).orElseThrow(() -> new NoSuchElementException(cpf));
+            List<Customer> customers = customerRepository.findCustomerByCPFContaining(cpf);
+
+            if (customers.isEmpty()) {
+                throw new NoSuchElementException("No users found with: " + cpf);
+            }
+
+            return customers;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Transactional(readOnly = true)
-    public Customer findCustomerById(UUID id) {
+    public List<Customer> findCustomerByEmailContaining(String email) {
         try {
-            return customerRepository.findById(id).orElseThrow(() -> new NoSuchElementException(id.toString()));
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-    }
+            List<Customer> customers = customerRepository.findCustomerByEmailContaining(email);
 
-    @Transactional(readOnly = true)
-    public Customer findCustomerByEmail(String email) {
-        try{
-            return customerRepository.findCustomerByEmail(email).orElseThrow(() -> new NoSuchElementException(email));
+            if (customers.isEmpty()) {
+                throw new NoSuchElementException("No users found with: " + email);
+            }
+
+            return customers;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Transactional(readOnly = true)
-    public Customer findCustomerByPhone(String phone) {
-        try{
-            return customerRepository.findCustomerByPhone(phone).orElseThrow(() -> new NoSuchElementException(phone));
+    public List<Customer> findCustomerByPhoneContaining(String phone) {
+        try {
+            List<Customer> customers = customerRepository.findCustomerByPhoneContaining(phone);
+
+            if (customers.isEmpty()) {
+                throw new NoSuchElementException("No users found with: " + phone);
+            }
+
+            return customers;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Customer> findCustomerByNameContaining(String name) {
+        try {
+            List<Customer> customers = customerRepository.findCustomerByNameContaining(name);
+
+            if (customers.isEmpty()) {
+                throw new NoSuchElementException("No users found with: " + name);
+            }
+
+            return customers;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Customer findCustomerByExactCPF(String exactCPF) {
+        try {
+            return customerRepository.findCustomerByExactCPF(exactCPF).orElseThrow(() -> new NoSuchElementException(exactCPF));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Customer findCustomerByExactEmail(String exactEmail) {
+        try {
+            return customerRepository.findCustomerByExactEmail(exactEmail).orElseThrow(() -> new NoSuchElementException(exactEmail));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Customer findCustomerByExactPhone(String exactPhone) {
+        try {
+            return customerRepository.findCustomerByExactPhone(exactPhone).orElseThrow(() -> new NoSuchElementException(exactPhone));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Customer findCustomerByExactName(String exactName) {
+        try {
+            return customerRepository.findCustomerByExactName(exactName).orElseThrow(() -> new NoSuchElementException(exactName));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -90,17 +150,16 @@ public class CustomerService {
     }
 
     @Transactional
-    public Customer updateCustomer(UUID id, CustomerRecordDto customerDTO) {
-        try{
-            Customer customer = customerRepository.findById(id).orElseThrow(() -> new NoSuchElementException(id.toString()));
+    public Customer updateCustomer(String cpf, CustomerRecordDto customerDTO) {
+        try {
+            Customer customer = customerRepository.findCustomerByExactCPF(cpf).orElseThrow(() -> new NoSuchElementException(cpf));
 
             customer.setEmail(customerDTO.email());
             customer.setPhone(customerDTO.phone());
 
             return customerRepository.save(customer);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
 }
