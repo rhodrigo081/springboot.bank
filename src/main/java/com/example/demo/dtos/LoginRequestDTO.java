924f5d7af0f057b03ae8b55b78f33c326f5c3b13
rhodrigo081 <rhodrigo.leandro@gmail.com>
2025-12-06 21:19:27 -0300

    fix: Correct DTOs

---
+++ src/main/java/com/example/demo/dtos/LoginRequestDTO.java
@@ -0,0 +1,6 @@
+package com.example.demo.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(@NotBlank(message = "Login is required") String login, @NotBlank(message = "Password is required") String password) {
+}
--- src/main/java/com/example/demo/dtos/LoginResquestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginResquestDTO(@NotBlank(message = "Login is required") String login, @NotBlank(message = "Password is required") String password) {
-}
